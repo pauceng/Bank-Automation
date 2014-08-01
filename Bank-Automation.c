43a60939af3b37bfa656c03b91b23c612495b96d
msxiyev <msxiyev@gmail.com>
2014-08-01 15:39:29 +0300

    Updated Project added some functions

--- Bank-Automation.c
+++ Bank-Automation.c
@@ -1,7 +1,45 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+//
+int choice = 0;
 
-int main()
+void CustomerTable(int );
+void GeneralMenu(int );
+
+struct customer
 {
-	/* code */
-	return 0;
+	char name;
+	char surname;
+	char adress;
+	char email;
+	int tephone[10];
+}table[200];
+
+main() {
+	printf("Wellcome to the BANK AUTOMATION\n");
+	GeneralMenu();
+	printf("What is your choice ? :\n", );
+	scanf("%d" ,&choice);
+	GeneralMenu(choice);
+}
+//
+void GeneralMenu(int choice) {
+	switch (choice) {
+		case 1:
+			printf("\t1.User Table\n"); break; 
+			CustomerTable();
+		case 2:
+			printf("\t2.Customer Table\n"); break; 
+		case 3:
+			printf("\t3.Account Table\n"); break; 
+		case 4:
+			printf("\t4.Transfer Money\n"); break;
+		case 5:
+			printf("\t5.> EXIT <\n"); break; 
+	}
+}
+//
+void CustomerTable() {
+	printf("Wellcome You now in Customer Table\n");
 }
